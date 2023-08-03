8319ab35b7415090a128c9f7ae85d62c666e24df
Alexandr Nikiforenko <NikiforenkoAS@i.ua>
2023-08-03 10:27:11 +0300

    Update settings and requirements.txt

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
